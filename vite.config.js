fc81e05e9a3fe1827aaf3aec081af1260cc809b9
Kaustubh-Dalvi1001 <kaustubhdalvi1001@gmail.com>
2025-08-01 12:06:58 +0530

    Configure project for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Postify-Frontend-Social-Media-Web-App-React-Vite-MockAPI-/',
   plugins: [react()],
 })
